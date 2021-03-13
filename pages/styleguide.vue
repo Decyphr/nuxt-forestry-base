1be8ab5d011e6819ea129fc7d784581bcc733df0
blakehartman <bhartman@clickrain.com>
2021-03-12 17:33:19 -0700

    Fixed content page creation issue

---
+++ pages/styleguide.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="text-center mb-3">
+      <h1>Styleguide</h1>
+      <h2>Base Styles for the Site</h2>
+    </div>
+    <div class="my-6 p-6 border-b-2 border-t-2 border-dashed">
+      <div class="p-3 text-center text-white bg-primary-dk">
+        <h2>Typography</h2>
+      </div>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      primary: {
+        dk: '#12232E',
+        md: '#007CC7',
+        lt: '#4DA8DA'
+      },
+      secondary: {
+        md: '#203647',
+        lt: '#EEFBFB'
+      }
+    },
+
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    }
+  },
+}
